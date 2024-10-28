b694c7aea3833551addd1df6b845e784c50bd94b
Anushree <anushree2002s@gmail.com>
2024-10-28 18:48:39 +0545

    bg coloring working out

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 $(document).ready(function(){       
     $('#welcomeModal').modal('show');
      }); 
+
+// document.querySelector('.no-thanks-btn').addEventListener('click', function(){
+//     $('#welcomeModal').modal('.hide');
+// })
